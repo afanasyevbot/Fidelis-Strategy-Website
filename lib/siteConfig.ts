5a0a0c304b231e798053aec3ffcaa83587c55a56
Matthew Afanasiev <afanasyev27@live.com>
2026-04-24 16:16:58 -0500

    feat: add Natural Earth theme tokens and fonts

---
+++ lib/siteConfig.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "Fidelis Strategy",
+  tagline: "Faithful. Loyal. Trustworthy.",
+  url: "https://fidelisstrategy.net",
+  email: "matthew@fidelisstrategy.net",   // TODO: confirm with user
+  bookingUrl: "https://cal.com/fidelis/discovery", // TODO: confirm
+  formspreeId: "xxxxxxxx",                // TODO: confirm
+  gaId: "G-XXXXXXX",                      // TODO: confirm
+  nav: [
+    { label: "What We Build", href: "/what-we-build" },
+    { label: "About",         href: "/about" },
+    { label: "Case Studies",  href: "/case-studies/paradise-capital" },
+    { label: "Contact",       href: "/contact" },
+  ],
+};
